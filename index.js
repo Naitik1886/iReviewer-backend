ee0051badf50750f5ceed064887d90282a6f2102
Naitik <mymailofficial1886@gmail.com>
2025-07-26 17:50:26 +0530

    second

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const PORT = process.env.PORT || 8080;
 const aiRoutes = require('./routes/ai.routes.js');
 const cors = require('cors');
 
-app.get('/', (req, res) => {
-return res.send('Hello, World!')
-})
+
 app.use(cors());
 
 app.use(express.json());
